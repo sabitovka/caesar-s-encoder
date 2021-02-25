13d732341dacf2c7ab81abf64eb4dd5bcfa61fac
adm-sabitovka <sabitovka@shkd.bizml.ru>
2021-02-25 13:41:15 +0300

    Добавлен дешифратор. Написан интерфейс взаимодействия с пользователем
    Разработан класс дешифратора, добавлены классы исключений.
    Разработаны классы для запуска приложения и ввод данных пользователем.
    Подготовка в релизу

---
+++ Decryptor/src/main/java/Decryptor.java
@@ -0,0 +1,78 @@
+import exceptions.KeyOutOfBoundException;
+import exceptions.NonCyrillicKeywordException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Decryptor {
+
+    private final List<Character> alphabetTemplate = new ArrayList<>();
+    private List<Character> cryptStringTemplate = new ArrayList<>(32);
+
+    private int key;// ключ
+    private String keyword; // ключевое слово
+
+    public Decryptor(int key, String keyword) {
+        if (key < 0 || key >= 32) {
+            throw new KeyOutOfBoundException("Key must be in 0..31");
+        }
+        keyword = keyword.replaceAll("\\s+", "").trim();
+        if (Stream.of(keyword.split("")).anyMatch(s -> !s.isEmpty() && (s.charAt(0) < 1040 || s.charAt(0) > 1103))) {
+            throw new NonCyrillicKeywordException("The keyword contains non cyrillic chars");
+        }
+        this.key = key;
+        this.keyword = keyword.toUpperCase();
+        setupCryptTemplate();
+    }
+
+    private void setupCryptTemplate() {
+
+        // заполняем массив алфватом (1040 - Код Юникод символа А, 1071 - Код символа Я)
+        for (int i = 1040; i <= 1071; i++) {
+            alphabetTemplate.add( (char) i );
+            cryptStringTemplate.add((char) 0);
+        }
+
+        // записываем ключевое слово
+        int pos = key;
+        for (int i = 0; i < keyword.length(); i++) {
+            if (!cryptStringTemplate.contains(keyword.charAt(i))) {
+                cryptStringTemplate.set(pos, keyword.charAt(i));
+                pos = pos+1 >= 32 ? 0 : pos+1;
+            }
+        }
+
+        // дописывваем остальны буквы алфавита
+        int indexOfAlphabet = 0;
+
+        while(pos != key || indexOfAlphabet != 32) {
+            if (!cryptStringTemplate.contains(alphabetTemplate.get(indexOfAlphabet))) {
+                cryptStringTemplate.set(pos, alphabetTemplate.get(indexOfAlphabet));
+                pos = pos+1 >= 32 ? 0 : pos+1;
+            }
+            indexOfAlphabet++;/* = indexOfAlphabet+1 >= 32 ? 0 : indexOfAlphabet+1*/
+        }
+
+        System.out.println("Шаблон алфавита: \t\t\t\t\t" + alphabetTemplate);
+        System.out.println("Шаблон зашифрованного алфавита: \t" + cryptStringTemplate);
+    }
+
+    public String decryptMessage(String message) {
+        // приводим к верхнему регистру, делим на массив
+        // собираем все в строку с помощью индексации по известным значениям
+        if (message.isEmpty()) {
+            return "";
+        }
+        return Stream.of(message.toUpperCase().split(""))
+                .reduce("", (s, s2) -> {
+                    s += (!s2.isEmpty() && cryptStringTemplate.indexOf(s2.charAt(0)) != -1) ?
+                            alphabetTemplate.get(cryptStringTemplate.indexOf(s2.charAt(0))) :
+                            " ";
+                    return s;
+                })
+                .replaceAll("\\s+", " ")
+                .trim();
+    }
+
+}
